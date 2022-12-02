dcb6b1ac4ffe8cfc819b2f424323470fed268d6b
jhordanvsqz <jhordan.vasquez@geor.pe>
2022-12-01 21:46:51 -0500

    primer commit

---
+++ tiposVariables.js
@@ -0,0 +1,20 @@
+//  NUMERICA
+ 
+ var número = 8;
+ var decimal = 3.5;
+
+// CADENA DE TEXTO
+var text ='Hola, aquí irá un texto';
+
+// BOOLEANA
+var boleano =true;
+var falso= false;
+
+// ELEMENTO HTMl
+var segundoParrafo=document.querySelector('#texto2')
+
+// ARREGLOS
+var familia = []
+
+// OBJETOS
+// FUNCIONES
---
+++ variables.js
@@ -0,0 +1,4 @@
+var nombre ="sergio";
+
+//alert(nombre);
+console.log(nombre);
